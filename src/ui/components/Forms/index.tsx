10116ba5d66143727c1521e391e687c1b8bed5ac
janio <sk8.merp@gmail.com>
2024-08-26 21:01:45 -0300

    att

--- src/ui/components/Forms/index.tsx
+++ src/ui/components/Forms/index.tsx
@@ -1,20 +1,22 @@
 "use client";
 import * as React from "react";
-import Box from "@mui/material/Box";
 import IconButton from "@mui/material/IconButton";
-import Input from "@mui/material/Input";
-import FilledInput from "@mui/material/FilledInput";
 import OutlinedInput from "@mui/material/OutlinedInput";
 import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
-import FormHelperText from "@mui/material/FormHelperText";
 import FormControl from "@mui/material/FormControl";
-import TextField from "@mui/material/TextField";
 import Visibility from "@mui/icons-material/Visibility";
 import VisibilityOff from "@mui/icons-material/VisibilityOff";
+import Checkbox from "@mui/material/Checkbox";
+import Link from "next/link";
 
 export default function InputAdornments() {
   const [showPassword, setShowPassword] = React.useState(false);
+  const [checked, setChecked] = React.useState(true);
+
+  const handleChange = (event: any) => {
+    setChecked(event.target.checked);
+  };
 
   const handleClickShowPassword = () => setShowPassword((show) => !show);
 
@@ -53,6 +55,17 @@ export default function InputAdornments() {
           label="Password"
         />
       </FormControl>
+      <div className="w-full flex items-center justify-center py-4">
+        <div className="w-2/4 flex items-center justify-start">
+          <Checkbox checked={checked} onChange={handleChange} inputProps={{ "aria-label": "controlled" }} />
+          <p>Mantenha-me conectado por 2 dias</p>
+        </div>
+        <div className="flex items-center justify-end w-2/4">
+          <Link className="underline" href="/esqueciminhasenha">
+            Esqueci minha senha
+          </Link>
+        </div>
+      </div>
       <button className="w-full px-4 py-4 bg-secondary my-4 text-primary font-sans text-lg font-bold">Entrar</button>
     </div>
   );
